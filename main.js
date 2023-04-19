4f78193d737af343c049e4a35857e6b17c0482d6
Lavender-wind <131239873+Lavender-wind@users.noreply.github.com>
2023-04-19 16:09:19 +0800

    created page

---
+++ main.js
@@ -0,0 +1,12 @@
+
+var youSuck = document.getElementById("you-suck");
+youSuck.onclick = function(){
+    console.log("Sasuke");
+}
+
+//1. Switching between backgrounds, screens, & character poses
+
+
+
+//2. Dialogue running along with plot progression
+//3. Player choices affecting ending and some plot
