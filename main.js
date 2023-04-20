7b746fa2a6338e10aa5b627076ebcf423b132186
Lavender-wind <131239873+Lavender-wind@users.noreply.github.com>
2023-04-20 11:55:32 +0800

    Attempted to make fading effect
    status: failed

--- main.js
+++ main.js
@@ -1,7 +1,32 @@
+var idCounter = 0;
 
-var youSuck = document.getElementById("you-suck");
-youSuck.onclick = function(){
-    console.log("Sasuke");
+class Screen{
+    constructor(img, textBox){
+        this.img = img;
+        this.textBox = textBox;
+    }
+    setScene(scene){
+        this.img.src = scene.img;
+        this.scene = scene;
+       // this.textBox.setText(scene.text);
+    }
+}
+
+class Scene{
+    constructor(img, text, character){
+        this.img = img;
+        this.text = text;
+        this.character = character;
+        this.id = idCounter;
+        idCounter++;
+    }
+}
+
+const screen = new Screen(document.getElementById("someImage"),undefined);
+
+var Start = document.getElementById("Start");
+Start.onclick = function(){
+    screen.setScene(new Scene("whateverthefuck","you suck"));
 }
 
 //1. Switching between backgrounds, screens, & character poses
